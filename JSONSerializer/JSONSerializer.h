0b76c78c100bb90edfb8875838fde3dadae75351
Mikhail Stepkin <mikhail.s@ramotion.com>
2016-04-08 11:56:15 +0300

    Optimizing Headers

---
+++ JSONSerializer/JSONSerializer.h
@@ -0,0 +1,18 @@
+//
+//  JSONSerializer.h
+//  JSONSerializer
+//
+//  Created by Mikhail Stepkin on 07.04.16.
+//  Copyright © 2016 Ramotion. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for JSONSerializer.
+FOUNDATION_EXPORT double JSONSerializerVersionNumber;
+
+//! Project version string for JSONSerializer.
+FOUNDATION_EXPORT const unsigned char JSONSerializerVersionString[];
+
+// In this header, you should import all the public headers of your framework
+// using statements like #import <JSONSerializer/PublicHeader.h>
